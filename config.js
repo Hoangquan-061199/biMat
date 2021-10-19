9ec6f95a9bd49b4a3763da7ba43d74b8938146ff
hoangquan-061199 <hoangquan061199@gmail.com>
2021-10-19 18:24:20 +0700

    up code

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  introTitle: "Chào em!!!",
+  introDesc: `Hôm nay anh có một chuyện muốn nói với em, em nghe hết nhé rồi trả lời anh nha!`,
+  btnIntro: "bấm vào đây để nghe tiếp nè!",
+  title:
+    "Hôm nay nhân ngày 20 tháng 10 anh chúc em và gia đình có một ngày thật vui vẻ, tràn đầy sức khoẻ và hạnh phúc nhé 😙",
+  desc: " Tối nay anh có thể mời em đi chơi một hôm được không?",
+  btnYes: "Oke anh, tối nay em rãnh <3",
+  btnNo: "Không,  anh trai à :v",
+  question: "Viết cho anh đôi lời nhé 😙",
+  btnReply: "Gửi cho anh <3",
+  reply: "Em cũng thích đi chơi với anh mà hihi 😘😘",
+  mess: "Anh biết mà 🥰. anh cũng thích đi chơi với em 😘😘",
+  messDesc: "Tối nay 7h30 anh qua đón nhé!",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/messages/t/100005903492519",
+};
